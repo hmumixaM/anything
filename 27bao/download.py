297e998f37d77de66ee0075212ce9c532d4ec665
hmumixaM <max@12450.top>
2019-07-29 17:54:52 +0800

    Initiate new project-27bao

---
+++ 27bao/download.py
@@ -0,0 +1,16 @@
+import json
+import urllib
+
+def simple_download():
+	fo = open("test.txt", "r+")
+	line = fo.readlines()
+	for i in xrange(0,len(line)):
+		js = json.loads(line[i])
+		for j in range(0,len(js["images"])):
+			url = str(js["images"][j][0])
+			print url
+			name = str(j) + "s" + str(i) + ".gif"
+			urllib.urlretrieve(url, name)
+	fo.close()
+
+simple_download()
