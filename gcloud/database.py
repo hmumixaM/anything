05d656798cbb8401207acfbac267b96fc3024b7d
hmumixaM <max@12450.top>
2020-02-13 22:31:56 -0500

    Service online

--- gcloud/database.py
+++ gcloud/database.py
@@ -2,9 +2,15 @@ import pymongo
 
 uri = "mongodb+srv://hello:qweasdZxc1@jandan-l7bmq.gcp.mongodb.net/code?retryWrites=true&w=majority"
 client = pymongo.MongoClient(uri)
+# client = pymongo.MongoClient(host='127.0.0.1', port=27017)
 db = client.anything
+# db = client.baola
 collection = db.gif
 
 def find(page):
-    a = collection.find()
+    a = collection.find({})
     return a[page]
+
+
+if __name__ == '__main__':
+    a = find(1)
--- gcloud/main.py
+++ gcloud/main.py
@@ -2,15 +2,43 @@
 from flask import *
 from database import find
 
-
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return "<a href='gif/1'>gif page</a>"
+
+
+@app.errorhandler(404)
+def error():
+    return redirect(url_for('hello'))
+
+
+@app.route('/gif')
+def other():
+    return redirect(url_for('gif', page=1))
+
+
+@app.route('/gif/<page>')
+def gif(page):
+    if not page.isdigit():
+        return redirect(url_for('gif', page=1))
+    page = int(page)
+    if page <= 0 or page > 1188:
+        return redirect(url_for('gif', page=1))
+    result = find(page-1)
+    title = result['title']
+    gifs = result['gif']
+    if page <= 4:
+        pages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    elif page > 1181:
+        pages = range(1179, 1189)
+    else:
+        pages = range(page-3, page+7)
+    return render_template('gif.html', page=page, pages=pages, title=title, gifs=gifs)
 
 
 if __name__ == '__main__':
